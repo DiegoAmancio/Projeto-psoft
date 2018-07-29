f730d18b46a20ce5fbd97d2f6b4cbac76e765d27
José Vinícius Lacerda de Arruda <jose.arruda@ccc.ufcg.edu.br>
2018-07-28 21:23:39 -0300

    fixing syntax errors

--- Scripts/login.js
+++ Scripts/login.js
@@ -6,17 +6,17 @@ Vue.use(GoogleAuth, { clientID: '415663373552-3foebghlgeuglmmaan8p0j0id0oi6vlq.a
 Vue.googleAuth().load();
 
 
-Vue.googleAuth().onSignIn(googleUser) {
+Vue.googleAuth().onSignIn(function(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-};
+});
 
-Vue.googleAuth().signOut() {
+Vue.googleAuth().signOut(function() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
     });
-};
+});
