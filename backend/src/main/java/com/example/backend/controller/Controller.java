85f4e61df254c861cd06a56199abb3da48bbb1fd
José Vinícius Lacerda de Arruda <jose.arruda@ccc.ufcg.edu.br>
2018-07-30 17:08:06 -0300

    Mostrando quem é que manda
    Agora o logout funciona

--- backend/src/main/java/com/example/backend/controller/Controller.java
+++ backend/src/main/java/com/example/backend/controller/Controller.java
@@ -1,6 +1,4 @@
 package com.example.backend.controller;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,67 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.backend.model.Aluno;
-import com.example.backend.model.Disciplina;
-import com.example.backend.service.AlunoService;
-
-import br.com.ufcg.springboot.service.DisciplinaService;
-
-
 @RestController
-@RequestMapping(value = "/api")
-@CrossOrigin(origins = "*")
-
 public class Controller {
-	
-	DisciplinaService disciplinaService;
-	AlunoService alunoService;
-	@RequestMapping(value = "/Aluno", method = RequestMethod.GET)
-	public Aluno cadastrarAluno(@RequestBody Aluno aluno) {
-		return alunoService.cadastrarAluno(aluno);
-	}
-	/**
-	 * METODO GET DE DISCIPLINA
-	 */
-	@RequestMapping(value = "/service", method = RequestMethod.GET)
-	public List<Disciplina> getAll() {
-		return disciplinaService.getAll();
-	}
-	/**
-	 * METODO POST DE DISCIPLINA
-	 */
-	@RequestMapping(value = "/service", method = RequestMethod.POST)
-	public Disciplina save(@RequestBody Disciplina disciplina) {
-		return disciplinaService.cadastardisciplina(disciplina);
-	}
-	/**
-	 * METODO POST DE DISCIPLINA PELO ID
-	 */
-	@RequestMapping(value = "/todo/{id}", method = RequestMethod.GET)
-	public Disciplina getById(@PathVariable("id") Long id) {
-		return disciplinaService.getById(id);
-	}
-	/**
-	 * METODO PUT DE DISCIPLINA
-	 */
-	@RequestMapping(value = "/service/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Disciplina> update(@PathVariable("id") Long id, @RequestBody Disciplina disciplina) {
-		Disciplina disciplina  = disciplinaService.update(disciplina, id);
-		return new ResponseEntity<Disciplina>(disciplina, HttpStatus.OK);
-	}
-	/**
-	 * METODO DELETE DE DISCIPLINA PELO ID
-	 */
-	@RequestMapping(value = "/service/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Disciplina> delete(@PathVariable("id") Long id) {
-		Disciplina disciplina = disciplinaService.delete(id);
-		return new ResponseEntity<Disciplina>(disciplina, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/service/search/{text}", method = RequestMethod.GET)
-	public List<Disciplina> searchByText(@PathVariable("text") String text) {
-		return disciplinaService.searchByText(text);
-	}
-	
-}
 
+}
