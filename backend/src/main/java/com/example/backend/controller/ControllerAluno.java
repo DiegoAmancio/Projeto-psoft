746fa49bdf0e7d55b8748caeefdcafb0ecf73b74
diegoamancio <diego.pereira@ccc.ufcg.edu.br>
2018-07-31 23:51:13 -0300

    renomeando controller para controllerAlunos passando responsabilidades que pegam disciplina para a nova classe controller e melhorando path

--- backend/src/main/java/com/example/backend/controller/ControllerAluno.java
+++ backend/src/main/java/com/example/backend/controller/ControllerAluno.java
@@ -21,9 +21,9 @@ import com.example.backend.service.DisciplinaService;
 import com.example.backend.service.InteressadoDService;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/aluno")
 @CrossOrigin(origins = "*")
-public class Controller {
+public class ControllerAluno {
 	@Autowired
 	DisciplinaService disciplinaService;
 	@Autowired
@@ -31,47 +31,21 @@ public class Controller {
 	@Autowired
 	private InteressadoDService alunosInteressados;
 
-	@RequestMapping(value = "/aluno", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public List<Aluno> todasMatriculas() {
 		return alunoService.todasMatriculas();
 	}
 
-	@RequestMapping(value = "/aluno", method = RequestMethod.POST)
+	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public Aluno cadastrarAluno(@RequestBody Aluno aluno) {
 
 		return alunoService.cadastrarAluno(aluno);
 
 	}
 
-	@RequestMapping(value = "/disciplinas", method = RequestMethod.GET)
-	public List<Disciplina> listar() {
-		return disciplinaService.disciplinasCadastradas();
-	}
-
-	@RequestMapping(value = "/aluno/disciplinas/{id}", method = RequestMethod.DELETE)
-	public HttpStatus delete(@PathVariable("id") Integer id) {
-		if (disciplinaService.deletarDisciplina(id)) {
-			return HttpStatus.OK;
-		}
-		return HttpStatus.NOT_FOUND;
-	}
-
 	/**
 	 * METODO PUT DE DISCIPLINA
 	 */
-	@RequestMapping(value = "/disciplinas", method = RequestMethod.PUT)
-	public HttpStatus update(@RequestBody Disciplina disciplina) {
-		Disciplina disciplinaAtt = disciplinaService.update(disciplina);
-		if (disciplinaAtt != null)
-			return HttpStatus.OK;
-		return HttpStatus.EXPECTATION_FAILED;
-	}
-
-
-	@RequestMapping(value = "/disciplinas", method = RequestMethod.POST)
-	public Disciplina save(@RequestBody Disciplina disciplina) {
-		return disciplinaService.cadastrarDisciplina(disciplina);
-	}
 
 	@RequestMapping(value = "/disciplinas/{matricula}", method = RequestMethod.GET)
 	public List<InteressadosDisciplina> cadeirasAluno(@PathVariable("matricula") String matricula) {
@@ -81,7 +55,8 @@ public class Controller {
 	}
 
 	@RequestMapping(value = "/disciplinas/{matricula}", method = RequestMethod.POST)
-	public HttpStatus cadastrar(@RequestBody Integer codigo, @PathVariable("matricula") String matricula) {
+	public HttpStatus cadastrarCadeiraInteresse(@RequestBody Integer codigo,
+			@PathVariable("matricula") String matricula) {
 
 		Optional<Disciplina> disciplina = disciplinaService.findById(codigo);
 		Optional<Aluno> aluno = alunoService.getAluno(matricula);
@@ -94,4 +69,5 @@ public class Controller {
 
 	}
 
+	
 }
--- backend/src/main/java/com/example/backend/service/DisciplinaService.java
+++ backend/src/main/java/com/example/backend/service/DisciplinaService.java
@@ -6,55 +6,49 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.backend.model.Disciplina;
-import com.example.backend.model.RequisitoDisciplina;
 import com.example.backend.repository.DisciplinasRepositorio;
 
 @Service
-public class DisciplinaService  {
+public class DisciplinaService {
 	@Autowired
 	private DisciplinasRepositorio disciplinas;
-	@Autowired
-	private RequisitosService requisitos;
-	
-	
+
 	public Disciplina cadastrarDisciplina(Disciplina novaDisciplina) {
 		Optional<Disciplina> disciplina = disciplinas.findById((int) novaDisciplina.getCodigo_disciplina());
-		
-		if(!disciplina.isPresent()) {
+
+		if (!disciplina.isPresent()) {
 			disciplinas.save(novaDisciplina);
-			
-			
+
 		}
 		return novaDisciplina;
-		
+
 	}
-	public List<RequisitoDisciplina> getRequisitos(Integer codigo_disciplina){
-		return requisitos.requisitosCadeira(codigo_disciplina);
-	}
-	public List<Disciplina> disciplinasCadastradas(){
+
+	public List<Disciplina> disciplinasCadastradas() {
 		return disciplinas.getAll();
 	}
 
 	public Disciplina update(Disciplina novaDisciplina) {
 		Optional<Disciplina> disciplinaNoBd = disciplinas.findById((int) novaDisciplina.getCodigo_disciplina());
-		if(disciplinaNoBd.isPresent()) {
+		if (disciplinaNoBd.isPresent()) {
 			disciplinas.deleteById((int) novaDisciplina.getCodigo_disciplina());
 			disciplinas.save(novaDisciplina);
 			return novaDisciplina;
 		}
 		return null;
 	}
+
 	public boolean deletarDisciplina(Integer id) {
 		Optional<Disciplina> disciplinaNoBd = disciplinas.findById(id);
-		if(disciplinaNoBd.isPresent()) {
+		if (disciplinaNoBd.isPresent()) {
 			disciplinas.deleteById(id);
 			return true;
 		}
 		return false;
 	}
+
 	public Optional<Disciplina> findById(Integer codigo_disciplina) {
-		// TODO Auto-generated method stub
 		return disciplinas.findById(codigo_disciplina);
 	}
-	
+
 }
