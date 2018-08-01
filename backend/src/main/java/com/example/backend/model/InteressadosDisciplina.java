ab7e34d0660859c91a7ccb821d00eedae5d2ea98
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2018-08-01 14:52:54 -0300

    tirando multiplas saidas na pesquisa de interessadosCadeira

--- backend/src/main/java/com/example/backend/model/InteressadosDisciplina.java
+++ backend/src/main/java/com/example/backend/model/InteressadosDisciplina.java
@@ -11,42 +11,38 @@ import javax.persistence.Table;
 @Table(name = "InteressadosDisciplina")
 public class InteressadosDisciplina {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private String matriculaDisciplina;
 	@Column(nullable = false)
 	private String aluno_matricula;
-	
+
 	@Column(nullable = false)
 	private Integer n_disciplina;
-	
+
 	public InteressadosDisciplina() {
-		
+
 	}
+
 	public InteressadosDisciplina(String aluno_matricula, Integer n_disciplina) {
 
 		this.aluno_matricula = aluno_matricula;
 		this.n_disciplina = n_disciplina;
+		this.matriculaDisciplina = aluno_matricula + n_disciplina;
 	}
-	
-	
+
 	public String getAluno_matricula() {
 		return aluno_matricula;
 	}
-
 
 	public void setAluno_matricula(String aluno_matricula) {
 		this.aluno_matricula = aluno_matricula;
 	}
 
-
 	public Integer getN_disciplina() {
 		return n_disciplina;
 	}
 
-
 	public void setN_disciplina(Integer n_disciplina) {
 		this.n_disciplina = n_disciplina;
 	}
-
 
 }
